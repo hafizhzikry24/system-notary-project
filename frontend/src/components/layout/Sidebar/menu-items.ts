05c6eed91adcb11ac8042bf7d16e74cd0e013c3b
hafizhzikry24 <hafizhcool24@gmail.com>
2025-08-09 00:34:25 +0700

    fix:demo sidebar menu

--- frontend/src/components/layout/Sidebar/menu-items.ts
+++ frontend/src/components/layout/Sidebar/menu-items.ts
@@ -1,6 +1,6 @@
 // src/lib/navItems.ts
 
-import { Home, Users, Shield } from "lucide-react";
+import { Home, Landmark, UserRoundPen, Banknote, LayoutList, Grid2x2Check } from "lucide-react";
 import { MenuItem } from "@/types/layout/sidebar";
 
 export const navItems: MenuItem[] = [
@@ -9,14 +9,52 @@ export const navItems: MenuItem[] = [
     href: "/dashboard",
     icon: Home,
   },
+  // currently unused
+  // {
+  //   title: "Roles",
+  //   href: "/role",
+  //   icon: Shield,
+  // },
   {
-    title: "Users",
-    href: "/users",
-    icon: Users,
+    title: "Pelanggan",
+    href: "/customers",
+    icon: Landmark,
+    children: [
+      { title: "Perorangan", href: "/perorangan" },
+      { title: "Bank & Leasing", href: "/bank" },
+      { title: "Perusahaan", href: "/perusahaan" },
+    ],
+  },
+    {
+    title: "Lembar Kerja",
+    href: "/worksheets",
+    icon: LayoutList,
+    children: [
+      { title: "Lembar Kerja", href: "/worksheet" },
+      { title: "Daftar Akta", href: "/list-akta" },
+    ],
   },
   {
-    title: "Roles",
-    href: "/role",
-    icon: Shield,
+    title: "Rekap Keuangan",
+    href: "/financials",
+    icon: Banknote,
+    children: [
+      { title: "Keuangan", href: "/keuangan" },
+      { title: "Kas & Dana Bank", href: "/kas" },
+    ],
+  },
+  {
+    title: "Master Data",
+    href: "/master-data",
+    icon: Grid2x2Check,
+    children: [
+      { title: "Partner", href: "/partner" },
+      { title: "Template Akta", href: "/template" },
+    ],
+  },
+  {
+    title: "Notaris & PPAT Profil",
+    href: "/profils",
+    icon: UserRoundPen,
   },
 ];
