1c195bd44715b6a06175827fdccb44dbf0d1561a
hafizhzikry24 <hafizhcool24@gmail.com>
2025-08-17 19:42:58 +0700

    fix:feat:crud backend for customer company

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -6,9 +6,11 @@ use Illuminate\Support\ServiceProvider;
 use App\Http\Repositories\AuthRepository;
 use App\Http\Repositories\RoleRepository;
 use App\Http\Repositories\ProfileSettingRepository;
+use App\Http\Repositories\CustomerCompanyRepository;
 use App\Http\Repositories\Interface\AuthRepositoryInterface;
 use App\Http\Repositories\Interface\RoleRepositoryInterface;
 use App\Http\Repositories\Interface\ProfileSettingRepositoryInterface;
+use App\Http\Repositories\Interface\CustomerCompanyRepositoryInterface;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);// Bind the AuthRepositoryInterface to AuthRepository
         $this->app->bind(RoleRepositoryInterface::class, RoleRepository::class);// Bind the RoleRepositoryInterface to RoleRepository
         $this->app->bind(ProfileSettingRepositoryInterface::class, ProfileSettingRepository::class); // Bind the ProfileSettingRepositoryInterface to ProfileSettingRepository
+        $this->app->bind(CustomerCompanyRepositoryInterface::class, CustomerCompanyRepository::class); // Bind the CustomerCompanyRepositoryInterface to CustomerCompanyRepository
     }
 
     /**
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\CustomerCompanyController;
 use App\Http\Controllers\ProfilesettingController;
 
 // endpoint for authentication
@@ -29,5 +30,14 @@ Route::middleware('auth:api')->group(function () {
         Route::get('/', [ProfilesettingController::class, 'show']);
         Route::get('/gender-options', [ProfilesettingController::class, 'getGenderValues']);
         Route::put('/', [ProfilesettingController::class, 'update']);
+    });
+
+    //customer company endpoint
+    Route::prefix('customer-companies')->group(function () {
+        Route::get('/', [CustomerCompanyController::class, 'index']);
+        Route::get('/{id}', [CustomerCompanyController::class, 'show']);
+        Route::post('/', [CustomerCompanyController::class, 'store']);
+        Route::put('/{id}', [CustomerCompanyController::class, 'update']);
+        Route::delete('/{id}', [CustomerCompanyController::class, 'destroy']);
     });
 });
