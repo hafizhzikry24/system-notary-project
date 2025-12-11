e15f2452d4131d3ed0e0bea90afa03e5214ef64c
hafizhzikry24 <hafizhcool24@gmail.com>
2025-12-12 00:08:13 +0700

    fix:move cred login to env

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    token_identifier: "sYsteMproJect@IdentFIer2025",
-    api_url: "http://127.0.0.1:8000/api"
-}
+    token_identifier: process.env.NEXT_PUBLIC_TOKEN_IDENTIFIER,
+    api_url: process.env.NEXT_PUBLIC_API_URL,
+};
